58bbeea3b4bf4f91a48f03b45092fdd1bcb18f3d
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-01 18:00:09 -0300

    Criando minha pasta controllers

---
+++ src/main/java/com/fuc/biblioteca/controllers/CategoriaController.java
@@ -0,0 +1,11 @@
+package com.fuc.biblioteca.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+
+}

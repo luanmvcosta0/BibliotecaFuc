6cab8e81105094dfc1b33d308951ef6301fa09db
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-01 18:16:38 -0300

    Adicionando a dependencia ModelMapper, criando a pasta e configurando o ModelMapper

--- src/main/java/com/fuc/biblioteca/controllers/CategoriaController.java
+++ src/main/java/com/fuc/biblioteca/controllers/CategoriaController.java
@@ -1,11 +1,19 @@
 package com.fuc.biblioteca.controllers;
 
+import com.fuc.biblioteca.models.Categoria;
+import com.fuc.biblioteca.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
 
 
 }
--- src/main/java/com/fuc/biblioteca/services/CategoriaService.java
+++ src/main/java/com/fuc/biblioteca/services/CategoriaService.java
@@ -1,7 +1,15 @@
 package com.fuc.biblioteca.services;
 
+import com.fuc.biblioteca.models.Categoria;
+import com.fuc.biblioteca.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+
 }

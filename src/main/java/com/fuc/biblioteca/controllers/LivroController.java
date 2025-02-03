c6bcd9ea7d48cf7cc848a1aa9928cf98df952c52
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-03 20:32:32 -0300

    Criando o metodo HTTP findAll dos livros

--- src/main/java/com/fuc/biblioteca/controllers/LivroController.java
+++ src/main/java/com/fuc/biblioteca/controllers/LivroController.java
@@ -1,14 +1,10 @@
 package com.fuc.biblioteca.controllers;
 
 import com.fuc.biblioteca.dtos.LivroDto;
-import com.fuc.biblioteca.models.Livro;
 import com.fuc.biblioteca.services.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +21,11 @@ public class LivroController {
     public ResponseEntity<LivroDto> findById(@PathVariable Integer id) {
         LivroDto livroDto = livroService.findByIdDto(id);
         return ResponseEntity.ok().body(livroDto);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<LivroDto>> findAll() {
+        return ResponseEntity.ok().body(livroService.findAll());
     }
 
 }
--- src/main/java/com/fuc/biblioteca/services/LivroService.java
+++ src/main/java/com/fuc/biblioteca/services/LivroService.java
@@ -1,6 +1,8 @@
 package com.fuc.biblioteca.services;
 
+import com.fuc.biblioteca.dtos.CategoriaDto;
 import com.fuc.biblioteca.dtos.LivroDto;
+import com.fuc.biblioteca.models.Categoria;
 import com.fuc.biblioteca.models.Livro;
 import com.fuc.biblioteca.repositories.LivroRepository;
 import org.modelmapper.ModelMapper;
@@ -8,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LivroService {
@@ -20,6 +25,7 @@ public class LivroService {
     private ModelMapper modelMapper;
 
 
+    //Service método findById e findByIdDto dos livros
     public Livro findById(Integer id) {
         return livroRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id do livro não encontrado."));
@@ -28,6 +34,15 @@ public class LivroService {
     public LivroDto findByIdDto(Integer id) {
         Livro livro = findById(id);
         return modelMapper.map(livro, LivroDto.class);
+    }
+
+
+    //Service método findAll dos livros
+    public List<LivroDto> findAll() {
+        return livroRepository.findAll()                                                         //Obtenho todos os livros do Banco
+                .stream()                                                                        //Converto a lista em uma Stream(fluxo de dados)
+                .map(obj -> modelMapper.map(obj, LivroDto.class))    //Transformo cada Livro em LivroDto
+                .collect(Collectors.toList());                                           //Converto de volta para uma lista
     }
 
 

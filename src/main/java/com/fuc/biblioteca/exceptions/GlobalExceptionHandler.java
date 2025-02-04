81207011e780cb6e6874dfa5e5c5a81fef95dff4
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-04 20:13:21 -0300

    Adicionando na pasta Global a exception BadRequest para requisao invalida, erro 400

--- src/main/java/com/fuc/biblioteca/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/fuc/biblioteca/exceptions/GlobalExceptionHandler.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    //Captura exceções de recurso não encontrado (erro 404)
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    //Capturo exceções de requisição invalida (erro 400)
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handlerBadRequest(BadRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 
